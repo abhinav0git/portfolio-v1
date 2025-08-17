f32bf5a3e42e41382bd55255ed83ad826f0933d9
Abhinav Singh <abhinav07c@gmail.com>
2025-08-17 22:56:27 +0530

    feat: ui fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,13 +8,20 @@ const Sidebar = () => {
     <aside className="sidebar">
       <div className="sidebar-content">
         <div className="profile-info">
-          <h1 className="name font-bold">Abhinav Singh</h1>
-          <p className="tagline font-medium">Bringing Ideas to Reality ✨</p>
+          <div className="profile-header">
+            <div className="profile-image">
+              <img src="/pfp.jpeg" alt="Abhinav Singh" />
+            </div>
+            <div className="profile-text">
+              <h1 className="name font-bold">Abhinav Singh</h1>
+              <p className="tagline font-medium">Bringing Ideas to Reality ✨</p>
+            </div>
+          </div>
           <p className="bio font-regular">
             Hey there 👋 <br />I'm a <strong>Software Developer</strong> who loves building intuitive and modern
             web experiences. I enjoy bringing thoughtful design and clean code together, making sure
             that what I build not only works well but also feels great to use. <br /><br />
-            With a strong foundation in <strong>full-stack development</strong> and a passion for <strong>UI/UX</strong>, I aim to
+            With a strong foundation in <strong>Full-stack development</strong> and a passion for <strong>UI/UX</strong>, I aim to
             create digital products that are both elegant and impactful.
           </p>
         </div>
